242afca32aa1a5cca09f36c896845b9228a709c5
Alberto2409 <agrj85@gmail.com>
2022-11-27 19:31:36 -0500

    se envia proyecto

---
+++ bandas.html/script.js
@@ -0,0 +1,91 @@
+
+
+let nombre1 = document.getElementById ("nombre1")
+let respuestaNombre1 = document.getElementById ("respuestaNombre1")
+
+
+nombre1.addEventListener ("click",function(){
+let respuestaUsuario1 = prompt ("escribe el nombre del artista")
+respuestaNombre1.innerText = respuestaUsuario1
+})
+
+
+let album1 = document.getElementById ("album1")
+let respuestaAlbum1 = document.getElementById ("respuestaAlbum1")
+
+
+album1.addEventListener ("click",function(){
+let respuestaUsuario2 = prompt ("album de la cancion")
+respuestaAlbum1.innerText = respuestaUsuario2
+})
+
+let cancion1 = document.getElementById ("cancion1")
+let respuestaCancion1 = document.getElementById ("respuestaCancion1")
+
+
+cancion1.addEventListener ("click",function(){
+let respuestaUsuario3 = prompt ("nombre de la cancion")
+respuestaCancion1.innerText = respuestaUsuario3
+})
+
+
+
+let nombre2 = document.getElementById ("nombre2")
+let respuestaNombre2 = document.getElementById ("respuestaNombre2")
+
+
+nombre2.addEventListener ("click",function(){
+let respuestaUsuario4 = prompt ("escribe el nombre del artista")
+respuestaCancion1.innerText = respuestaUsuario4
+})
+
+
+let album2 = document.getElementById ("album2")
+let respuestaAlbum2 = document.getElementById ("respuestaAlbum2")
+
+
+album2.addEventListener ("click",function(){
+let respuestaUsuario5 = prompt ("album de la cancion")
+respuestaAlbum2.innerText = respuestaUsuario5
+})
+
+let cancion2 = document.getElementById ("cancion2")
+let respuestaCancion2 = document.getElementById ("respuestaCancion2")
+
+
+cancion2.addEventListener ("click",function(){
+let respuestaUsuario6 = prompt ("nombre de la cancion")
+respuestaCancion2.innerText = respuestaUsuario6
+})
+
+
+let nombre3 = document.getElementById ("nombre3")
+let respuestaNombre3 = document.getElementById ("respuestaNombre3")
+
+
+nombre3.addEventListener ("click",function(){
+let respuestaUsuario7 = prompt ("escribe el nombre del artista")
+respuestaNombre3.innerText = respuestaUsuario7
+})
+
+
+
+let album3 = document.getElementById ("album3")
+let respuestaAlbum3 = document.getElementById ("respuestaAlbum3")
+
+
+album3.addEventListener ("click",function(){
+let respuestaUsuario8 = prompt ("album de la cancion")
+respuestaAlbum3.innerText = respuestaUsuario8
+})
+
+
+let cancion3 = document.getElementById ("cancion3")
+let respuestaCancion3 = document.getElementById ("respuestaCancion3")
+
+
+cancion3.addEventListener ("click",function(){
+let respuestaUsuario9 = prompt ("nombre de la cancion")
+respuestaCancion3.innerText = respuestaUsuario9
+})
+
